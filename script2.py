4b04d4dd1302bbde2e5c9066fecec4fbe2b75240
sauljimenez <sauljv04avila@email.com>
2026-07-07 14:11:21 +0200

    Añadidos los scripts, el 1 finalizado y el 2 por hacer

--- script2.py
+++ script2.py
@@ -8,26 +8,29 @@ tareas_proyecto = [
     {"id": 5, "descripción": "Miniproyecto semana 1", "encargado": "Saúl", "completada": False},
     {"id": 6, "descripción": "Evaluación final", "encargado": "Suso", "completada": False}
 ]
-
+print("-----REGISTRO DE TAREAS-----")
 # Comprobamos si el encargado es "Saúl" y si la tarea no está completada, imprimir id y descripción si asi es
 print("Estas son las tareas que Saúl no ha realizado todavia:")
 for i in tareas_proyecto:
     if i["encargado"] == "Saúl" and i["completada"] == False:
         print(f"Id {i['id']} y descripción: {i['descripción']}.")
 
-# Procedemos a cambiar la tarea realizada ahora como completada
-id_a_cambiar = int(input("Introduce el id de la tarea completada: "))
+while True:
+    eleccion = input("¿Quieres registrar alguna tarea completada recientemente?: ").strip().lower()
+    if eleccion in ["sí", "si"]:
+        # Procedemos a cambiar la tarea realizada ahora como completada
+        id_a_cambiar = int(input("Introduce el id de la tarea completada: "))
 
-for i in tareas_proyecto:
-    if i["id"] == id_a_cambiar:
-        i["completada"] = True
-
-
-
-# RE RETO 3: Usa un bucle 'for' con el método '.items()' para mostrar los 
-# datos de la tarea modificada línea por línea con el formato "Clave -> Valor".
-# Pista del formato:
-# for clave, valor in diccionario_modificado.items():
-#     print(f"{clave} -> {valor}")
-
-# Tu código aquí...
+        for i in tareas_proyecto:
+            if i["id"] == id_a_cambiar:
+                i["completada"] = True
+        
+                print("Resumen de la actualización en el sistema:")
+                for clave, valor in i.items():
+                 print(f"{clave.capitalize()}: {valor}")
+                break
+    elif eleccion == "no":
+        print("Registro perfectamente actualizado.")
+        break
+    else:
+        print("Respuesta no valida. Escribe 'si' o 'no'.")
