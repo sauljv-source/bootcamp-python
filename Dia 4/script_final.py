6c117db10deb41787274a8165ad1509cd42a90b7
sauljimenez <sauljv04avila@email.com>
2026-07-09 13:12:28 +0200

    Actualizacion de varios scripts

--- Dia 4/script_final.py
+++ Dia 4/script_final.py
@@ -3,17 +3,6 @@
 import requests
 import pandas as pd
 
-def comprobar_texto(mensaje, error_mensaje, comas=False):
-    while True:
-        entrada = input(mensaje).strip()
-        texto_a_validar = entrada.replace(" ", "")
-        if comas:
-            texto_a_validar = texto_a_validar.replace(",", "")
-            
-        if texto_a_validar.isalpha() and entrada:
-            return entrada
-        print(error_mensaje)
-
 def comprobar_entero_entre_1_y_10(mensaje, error_mensaje1, error_mensaje2):
     while True:
         try:
@@ -43,20 +32,21 @@ cabeceras = {
     'Content-Type': 'application/json; charset=utf-8'
 }
 
-busqueda_usuario = True
-while busqueda_usuario:
-    id = comprobar_entero_entre_1_y_10("Introduce el id del usuario de la pagina web que quieres modificar (del 1 al 10): ", "El valor tiene que estar entre 1 y 10", "El dato introducido no es un valor válido.\nInténtalo de nuevo.")
+
+while True:
+    id_usuario = comprobar_entero_entre_1_y_10("Introduce el id del usuario de la pagina web que quieres modificar (del 1 al 10): ", "El valor tiene que estar entre 1 y 10", "El dato introducido no es un valor válido.\nInténtalo de nuevo.")
 
     try:
-        r = requests.get(f'{url}/{id}')
+        r = requests.get(f'{url}/{id_usuario}')
         print(f"Código de estado: {r.status_code}")
     
         if r.status_code == 200:
             print("Contenido de la id introducida:")
-            print(pd.json_normalize(r.json()).T)
+            df = pd.json_normalize(r.json())
+            print(df.T)
             comprobacion = true_false("¿Es esta la id que quieres modificar? (si o no): ", "Respuesta no válida. Escribe 'sí' o 'no'.")
             if comprobacion:
-                busqueda_usuario = False
+                break
             else:
                 print("De acuerdo, busquemos otro id.\n")
             
@@ -68,7 +58,7 @@ while busqueda_usuario:
     except Exception as e:
         print(f"Error inesperado: {e}")
 
-entradas = {'id', 
+entradas = [ 
     'name', 
     'username', 
     'email', 
@@ -83,31 +73,48 @@ entradas = {'id',
     'company.name', 
     'company.catchPhrase', 
     'company.bs'
-}
+]
 
 while True:
-    cambio = input("¿Que quieres modificar?: ")
-    if cambio.issubset(entradas):
+    cambio = input("¿Que entrada quieres modificar?: ").strip()
+    if cambio in entradas:
+        break
+    elif cambio == 'id':
+        print("No es posible cambiar el id.\nIntroduce otra entrada.")
+    else:
+        print("La entrada escrita no existe en el registro de datos.\nIntroduce una entrada que si exista para modificarla.")
         
-        
+      
+modificacion = input("Introduce el nuevo texto que quieras guardar: ")
 
+# Vamos a la posicion exacta de la tabla que hay que cambiar
+df.at[0, cambio] = modificacion
+
+# Pasamos los datos de la fila 0 a formato diccionario
+datos_planos = df.iloc[0].to_dict()
 
 nuevo_post = {
-    'id': 'df.loc[0, "id"]',
-    'name': 'df.loc[0, "name"]',
-    'username': 'df.loc[0, "username"]',
-    'email': 'df.loc[0, "email"]',
-    'phone': 'df.loc[0, "phone"]',
-    'website': 'df.loc[0, "website"]',
-    'address.street': 'df.loc[0, "address.street"]',
-    'address.suite': 'df.loc[0, "address.suite"]',
-    'address.city': 'df.loc[0, "address.city"]',
-    'address.zipcode': 'df.loc[0, "address.zipcode"]',
-    'address.geo.lat': 'df.loc[0, "address.geo.lat"]',
-    'address.geo.lng': 'df.loc[0, "address.geo.lng"]',
-    'company.name': 'df.loc[0, "company.name"]',
-    'company.catchPhrase': 'df.loc[0, "company.catchPhrase"]',
-    'company.bs': 'df.loc[0, "company.bs"]'
+    "id": datos_planos["id"],
+    "name": datos_planos["name"],
+    "username": datos_planos["username"],
+    "email": datos_planos["email"],
+    "phone": datos_planos["phone"],
+    "website": datos_planos["website"],
+    "address": {
+        "street": datos_planos["address.street"],
+        "suite": datos_planos["address.suite"],
+        "city": datos_planos["address.city"],
+        "zipcode": datos_planos["address.zipcode"],
+        "geo": {
+            "lat": datos_planos["address.geo.lat"],
+            "lng": datos_planos["address.geo.lng"]
+        }
+    },
+    "company": {
+        "name": datos_planos["company.name"],
+        "catchPhrase": datos_planos["company.catchPhrase"],
+        "bs": datos_planos["company.bs"]
+    }
 }
 
 try:
@@ -116,7 +123,9 @@ try:
     print(f"Código de estado del servidor: {r.status_code}")
     if r.status_code == 201:
         print("\nDatos actualizados con exito")
-        print(pd.DataFrame([r.json()]).T)
+        respuesta_servidor = r.json()
+        respuesta_servidor['id'] = id_usuario
+        print(pd.json_normalize(respuesta_servidor).T)
     else:
         print(f"El servidor rechazó la petición. Estado: {r.status_code}")
 
@@ -124,3 +133,8 @@ except requests.exceptions.RequestException as e:
     print(f"Error de red: {e}")
 except Exception as e:
     print(f"Error inesperado: {e}")
+    
+# Los datos no se van a quedar guardados en la web
+# Si volvemos a pedirlos van a ser los datos originales
+# Es una web publica para practicar que no permite postear cosas nuevas
+# Lo que hemos hecho es hacer un nuevo post modificando el id para que parezca una actualizacion de datos ya escritos
