b050505bb5b579d843b1f85dc98720330e834b91
sauljimenez <sauljv04avila@email.com>
2026-07-13 10:14:11 +0200

    Conexion segura a MongoDB usando dotenv

--- Dia 6/prueba_pymongo.py
+++ Dia 6/prueba_pymongo.py
@@ -1,13 +1,19 @@
 #Comprobacion de que este bien instalado y la versión sea la adecuada
 
+import os
 import pymongo
 from pymongo import MongoClient
+from dotenv import load_dotenv
+
+# Cargamos las variables ocultas del archivo .env para evitar compartir nuestro ususario y contraseña
+load_dotenv()
 
 print("Instalación realizada correctamente")
 print(f"Versión actual: {pymongo.__version__}")
 
-# Creamos nuestro orimer cluster en MongoDB Atlas y accedemos a el desde phyton
-MONGO_URI = "mongodb+srv://sauljv04:Bilbao35avila@cluster0.fduhqvc.mongodb.net/?appName=Cluster0"
+# Creamos nuestro primer cluster en MongoDB Atlas y accedemos a el desde phyton
+# Python lee la URL en memoria sin que nadie pueda ver la contraseña en el código
+MONGO_URI = os.getenv("MONGO_URI")
 
 try:
     client = MongoClient(MONGO_URI)
