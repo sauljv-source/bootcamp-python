b6811008ac32e7b0911e2c4cfb2e1620f0b70147
sauljimenez <sauljv04avila@email.com>
2026-07-13 10:06:31 +0200

    Script para comprobar la conexion exitosa a MongoDB Atlas

--- Dia 6/prueba_pymongo.py
+++ Dia 6/prueba_pymongo.py
@@ -1,6 +1,17 @@
 #Comprobacion de que este bien instalado y la versión sea la adecuada
 
 import pymongo
+from pymongo import MongoClient
 
 print("Instalación realizada correctamente")
 print(f"Versión actual: {pymongo.__version__}")
+
+# Creamos nuestro orimer cluster en MongoDB Atlas y accedemos a el desde phyton
+MONGO_URI = "mongodb+srv://sauljv04:Bilbao35avila@cluster0.fduhqvc.mongodb.net/?appName=Cluster0"
+
+try:
+    client = MongoClient(MONGO_URI)
+    client.admin.command('ping')
+    print("Conexión exitosa a MongoDB Atlas")
+except Exception as e:
+    print(f"Error de conexión: {e}")
