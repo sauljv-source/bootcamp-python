113503495312ee58a2440e65704cdc93186c03ed
sauljimenez <sauljv04avila@email.com>
2026-07-15 13:30:18 +0200

    Scripts completos dia 8 Consultas avanzadas MongoDB

--- Dia 8/consultas_avanzadas.py
+++ Dia 8/consultas_avanzadas.py
@@ -4,13 +4,10 @@ import os
 from dotenv import load_dotenv
 from conexion import obtener_coleccion
 
-def comprobar_texto(mensaje, error_mensaje, comas=False):
+def comprobar_texto(mensaje, error_mensaje):
     while True:
         entrada = input(mensaje).strip()
-        texto_a_validar = entrada.replace(" ", "")
-        if comas:
-            texto_a_validar = texto_a_validar.replace(",", "")
-            
+        texto_a_validar = entrada.replace(" ", "")     
         if texto_a_validar.isalpha() and entrada:
             return entrada
         print(error_mensaje)
@@ -23,11 +20,11 @@ def consultas_avanzadas():
         # Esctrictamente mas de n asignaturas aprobadas n solo no vale), filtro_gt es un conjunto de diccionaarios, los alumnos
         while True:
             try:
-                n = int(input("\nIntroduce la cota minima de asignaturas: ").strip())
-                if 0 <= n <= 40:
+                n = int(input("\nIntroduce la cota minima de asignaturas no contenida: ").strip())
+                if 0 <= n < 40:
                     break
                 else:
-                    print("El valor tiene que ser un numero entero y estar entre 0 y 40.")
+                    print("El valor tiene que estar entre 0 y 40.")
             except ValueError:
                 print("El valor introducido tiene que ser numerico.")
         print(f"\n----- Estudiantes con más de {n} asignaturas aprobadas ($gt) -----")
@@ -35,6 +32,24 @@ def consultas_avanzadas():
         for est in coleccion.find(filtro_gt):
             # .get('nombre') en vez de ['nombre'] para que no se rompa el programa si no hay campo nombre definido en algun usuario
             print(f"- {est.get('nombre')}: {est.get('asignaturas_aprobadas')} asignaturas")
+            
+        # $eq, condicion de igualdad
+        while True:
+            try:
+                age = int(input("\nIntroduce la edad del estudiante a buscar: ").strip())
+                if 0 <= age:
+                    break
+                print("La edad no puede ser un numero negativo")
+            except ValueError:
+                print("La edad introducida tiene que ser un numero.")
+        print(f"\n----- Estudiantes con {age} años ($eq) -----")
+        filtro_eq = {"edad": {"$eq": age}}
+        estudiantes = list(coleccion.find(filtro_eq))
+        if estudiantes:
+            for est in estudiantes:
+                print(f"- {est.get('nombre')}: {est.get('edad')} años")
+        else:
+            print(f"No se encontró ningún estudiante con {age} años")
 
         # $in, dentro de una lista
         print("\n----- Estudiantes en Matematicas o Física ($in) -----")
@@ -61,7 +76,7 @@ def consultas_avanzadas():
 
         # Con la proyección decidimos que campos queremos (1 para incluir, 0 para excluir) ya que MongoDB nos devuelve todos incluidos el id
         # No se puede mezclar 1 y 0 en la proyección, excepto para excluir el _id
-        print("\n----- Proyección: Solo queremos el nombre y licenciatura (excluyendo el _id y los demas campos) de los primeros 5 alumnos ingresados al sistema -----")
+        print("\n----- Proyección: Solo queremos el nombre y licenciatura (excluyendo el _id y los demas campos) de 5 alumnos ingresados al sistema -----")
         proyeccion = {"nombre": 1, "licenciatura": 1, "_id": 0}
         # Tomamos el conjunto completo (nuestra base de datos), proyeccion aplica el filtro de campos y .limit(5) nos reduce los resultados mostrados a solo 5 aunque haya mas
         for est in coleccion.find({}, proyeccion).limit(5):
